6ef727d9e4b8b3601096f54c1439227ee53d1bf2
Maharij Tariq <maharijtariq@gmail.com>
2026-04-18 00:14:41 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,37 @@
+function bulbonn1() {
+    document.getElementById("bulb").src = "bulbonn1.png";
+    document.body.style.background = "radial-gradient(circle, #fafa04, black)";
+}
+function bulboff1() {
+    document.getElementById("bulb").src = 'bulboff1.png';
+    document.body.style.background = "radial-gradient(circle, #353232, black)";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bulbon(){
+//     document.getElementById("bulb").src="bulbon.jpg";
+// }
+// function bulboffobj(){
+//     document.getElementById("bulb").src="bulboff.jpeg";
+// }
